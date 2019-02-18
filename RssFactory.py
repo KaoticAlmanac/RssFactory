872363b77f587a96ad4f44b4e4e9ea2ea7d2f582
Kevin Brown <kevin.brown.cs@gmail.com>
2019-02-17 23:29:00 -0500

    Have working prototype of the project.

--- RssFactory.py
+++ RssFactory.py
@@ -1,29 +1,37 @@
 from RssParser import RssParser
 import requests
+import datetime
 
 
 def get_rss_list():
     """This will call mongo to grab the RSS list that will likely be populated by hand.
        In the future I plan to have an alert if a new rss becomes available.
        Although for now I will just have a couple for testing"""
-    return ""
+    return "https://searchdatacenter.techtarget.com/rss/IT-infrastructure-news.xml\n" \
+           "https://searchdatacenter.techtarget.com/es/editorspicks\n" \
+           "https://www.lemagit.fr/rss/ContentSyndication.xml\n" \
+           "https://www.searchnetworking.de/rss/Alle-Artikel-und-News-von-SearchNetworkingDE.xml"
 
 
 def get_last_article(rss_feed):
     # type: (str) -> dict
-    # if None return
-    return {}
+    # if None return {'title':'','pubDate':datetime.datetime.min,'link':rss_feed}
+    return {'title': '', 'pubDate': datetime.datetime.min, 'link': rss_feed}
 
 
 def get_new_xml(rss_list):
     # type: (str) -> list
+    """Gets the new articles from the xml links
+       :param rss_list link to rss page
+       :return list of new articles"""
     parsed_rss_list = []
-    for rss in rss_list:
+    for rss in rss_list.splitlines():  # Splitlines is because each link will be delineated by it
+        print(rss)
         response = requests.get(rss)  # Gets the rss
 
         # Creates the parser that will parse the xml. Makes sure that the xml can be parsed, throws error otherwise
         parser = RssParser(response.content)
-        last_article=get_last_article(rss)  # Gets the last article saved on the server
+        last_article = get_last_article(rss)  # Gets the last article saved on the server
         # Parses the xml until I get a past article or I go past the previous time limit for an article
         # The previous time is in case the latest article is deleted
         # todo: Once a day or so validate everything
@@ -31,9 +39,19 @@ def get_new_xml(rss_list):
     return parsed_rss_list
 
 
+def insert_new_xml(parsed_rss_list):
+    # type: (list[RssParser]) -> None
+    """Inserts the parsed rss into the database"""
+    print(parsed_rss_list)
+    return
+
+
 def factory_start():
     rss_list = get_rss_list()
     parsed_rss_list = get_new_xml(rss_list)
-    # iterate through list and upload all the new articles
+    insert_new_xml(parsed_rss_list)  # iterate through list and upload all the new articles
 
 
+if __name__ == '__main__':
+    # todo: for arg in sys.argv[1]: --gives command to either run regular or validate entire xml
+    factory_start()
--- RssParser.py
+++ RssParser.py
@@ -1,4 +1,4 @@
-import xml.etree.cElementTree as ET
+import xml.etree.ElementTree as ET
 import datetime
 
 
@@ -8,8 +8,12 @@ class RssParser(object):
     def __init__(self,rss):
         """Makes sure that rss is valid"""
         try:
-            tree = ET.parse(rss)
+            print("Creating RssParser")
+            parser = ET.XMLParser(encoding='utf-8')
+            tree = ET.fromstring(rss, parser=parser)
         except Exception as e:
+            print("Error trying to parse rss")
+            #print(rss)
             print (e.message)
             raise e
 
@@ -34,19 +38,19 @@ class RssParser(object):
         """Parses through the xml until it matches a title or the publish date is less than or equal"""
         # todo: check to see if the title changed of an article
         list_of_items = []
-        root = self.tree.getroot()
+        root = self.tree
 
-        for item in root.findall('item'): # Only gets the item elements because that's all that matters
-            if item.find('title') == recent_item['title']:
+        for item in root.iter('item'): # Only gets the item elements because that's all that matters
+            if item.find('title').text == recent_item['title']:
                 break
-            if self._convert_time(item.find('pubDate')) < recent_item['pubDate']:
+            if self._convert_time(item.find('pubDate').text) < recent_item['pubDate']:
                 # alert this should only happen if something was deleted
                 # todo: create alert system to email me or something...
                 # todo: create validation system so that everything is reset and it is set to this
                 break
             list_of_items.append({"title":item.find('title').text,
-                                  "link":item.find('link'),
-                                  "pubDate":item.find('pubDate')})
+                                  "link":item.find('link').text,
+                                  "pubDate":item.find('pubDate').text})
         self.items = list_of_items
         return list_of_items
 
