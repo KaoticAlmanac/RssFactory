693a6f0fea48838bb17746cd69a7cb2b46fd158e
Kevin Brown <kevin.brown.cs@gmail.com>
2019-02-17 19:45:11 -0500

    Iterated through the list and parsed out the new information

--- RssFactory.py
+++ RssFactory.py
@@ -11,14 +11,29 @@ def get_rss_list():
 
 def get_last_article(rss_feed):
     # type: (str) -> dict
+    # if None return
     return {}
+
+
+def get_new_xml(rss_list):
+    # type: (str) -> list
+    parsed_rss_list = []
+    for rss in rss_list:
+        response = requests.get(rss)  # Gets the rss
+
+        # Creates the parser that will parse the xml. Makes sure that the xml can be parsed, throws error otherwise
+        parser = RssParser(response.content)
+        last_article=get_last_article(rss)  # Gets the last article saved on the server
+        # Parses the xml until I get a past article or I go past the previous time limit for an article
+        # The previous time is in case the latest article is deleted
+        # todo: Once a day or so validate everything
+        parsed_rss_list.append(parser.parse_until_point(last_article))
+    return parsed_rss_list
 
 
 def factory_start():
     rss_list = get_rss_list()
-    parsed_rss_list= []
-    for rss in rss_list:
-        response = requests.get(rss)
-        parser = RssParser(response.content)
-        last_article=get_last_article(rss)
-        parsed_rss_list.append(parser.parse_until_point({}))
+    parsed_rss_list = get_new_xml(rss_list)
+    # iterate through list and upload all the new articles
+
+
